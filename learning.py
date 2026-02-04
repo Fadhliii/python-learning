cddc68ce030143af2763e6e1677bda588b4f4ab5
Fadhli M <fadhli2316@gmail.com>
2026-02-04 14:30:04 +0700

    day 4 | suit

--- learning.py
+++ learning.py
@@ -1,1 +1,25 @@
-print("A")
+import random
+
+pilihan = ["gunting", "batu", "kertas"]
+bot = random.choice(pilihan)
+
+user = input("your choice: ").lower()
+
+if user not in pilihan:
+    print("invalid input")
+    exit()
+
+if user == bot:
+    result = "draw"
+elif user == "batu" and bot == "kertas":
+    result = "lose"
+elif user == "gunting" and bot == "batu":
+    result = "lose"
+elif user == "kertas" and bot == "gunting":
+    result = "lose"
+else:
+    result = "win"
+
+print("user:", user)
+print("bot :", bot)
+print("result:", result)
