2c603cfac023c2dd753c79c029b175f65092ce30
Fadhliii <fadhlimuhammad.main@gmail.com>
2026-02-05 23:47:59 +0700

    day  5 | final |

--- learning.py
+++ learning.py
@@ -10,14 +10,19 @@ n_numbers = int(input("choose jumlah nomor  : "))
 symbols = list(string.punctuation)
 n_symbols = int(input("choose jumlah simbol : "))
 
-pw = ""
+pw = []
 for char in range(0, n_letters):
-    pw += random.choice(letters)
+    pw.append(random.choice(letters))
     
 for char in range(0, n_numbers):
-    pw += random.choice(numbers)
+    pw.append(random.choice(numbers))
     
 for char in range(0, n_symbols):
-    pw += random.choice(symbols)
+    pw.append(random.choice(symbols))
     
 print(pw)
+random.shuffle(pw)
+print(pw) #shuffling result
+
+final_password = ''.join(pw)
+print(final_password)
